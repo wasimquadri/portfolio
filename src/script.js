3345408e56815d3971664b750c07482ec4eff89c
wasim <wasimlucky099@gmail.com>
2023-10-14 12:55:45 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+let currentValue = '';
+
+function appendToDisplay(value) {
+    currentValue += value;
+    display.value = currentValue;
+}
+
+function clearDisplay() {
+    currentValue = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        currentValue = eval(currentValue);
+        display.value = currentValue;
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
